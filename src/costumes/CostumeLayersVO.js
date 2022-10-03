9590b947f1e921d95dda6ca813398a91a2e1f6ee
WindowsTV <ryan1439@gmail.com>
2022-10-02 23:12:19 -0400

    Update CostumeLayersVO.js

--- src/costumes/CostumeLayersVO.js
+++ src/costumes/CostumeLayersVO.js
@@ -4,57 +4,48 @@ this.sjs = window.sjs || {};
 (function() {
 	"use strict";
 	// constructor:
+	var p = CostumeLayersNewVO.prototype; 	
 	/**
-	 * The CostumeLayersVO is a class to create layer information for a COSTUME
-	 * @class CostumeLayersVO
-	 * @param layer1{createjs Sprite}
-	 * @param layer2{createjs Sprite}
-	 * @param layer3{createjs Sprite}
-	 * @param layer4{createjs Sprite}
+	 * The CostumeLayersNewVO is a class to create layer information for a COSTUME
+	 * @class CostumeLayersNewVO
+	 * @param costumeObject{Object}
 	 * @constructor
 	 */
-	function CostumeLayersVO(costumeObject) {	
+	function CostumeLayersNewVO(costumeObject) {	
 		// public properties:	
-		var _theLayerNumber = 1;
-		for(var i = 0; i <= costumeObject.length; i++){	
-			//Remove isNewType in favor for a check to see if the Costume has layers 
-			//Consider using CreateJS's children instead of custom layers
-			if(costumeObject[i] && costumeObject[i].isNewType && costumeObject[i].isNewType == true)continue;
+		this._layers = [];
+		this._layers._length = 0;
+		
+		for(var i = 0; i <= costumeObject.length-1; i++){	
+			if(costumeObject[i] && costumeObject[i].isNewType && costumeObject[i].isNewType == true) continue;
 			if(costumeObject[i] && costumeObject[i].s != undefined){
-					this["_layer" + _theLayerNumber] = costumeObject[i].s;					
-					if(this["_layer" + _theLayerNumber] != undefined) {
-						this["_layer" + _theLayerNumber].name = ["layer" + _theLayerNumber];	
-						this["_layer" + _theLayerNumber].type = costumeObject[i].t;	
-						/*if(this.isADynamicLayer(this["_layer" + _theLayerNumber].type)){
-							this["_layer" + _theLayerNumber].isDynamic = true;	
-						}*/
-					}
-					_theLayerNumber++;
-			}
-		}
-		this._amount = (_theLayerNumber - 1);
+				let _sprite = costumeObject[i].s;
+				_sprite.name = ("layer" + i);	
+				_sprite.type = costumeObject[i].t;	
+				this._layers["_layer" + i] = _sprite;
+			}//END IF
+			this._layers._length = i;
+		}//END LOOP
 		return this;
 	}
-	var p = CostumeLayersVO.prototype; 
  
 	// public methods:
 	p.addLayer = function(sprite, layerNumber = -1){
 		if(!sprite){
-			console.warn("[CostumeLayersVO] NO SPRITE WAS GIVEN for method 'addLayer'. Stoping method..");
+			console.warn("[CostumeLayersNewVO] NO SPRITE WAS GIVEN for method 'addLayer'. Stoping method..");
 			return;
 		}
-		console.debug("[CostumeLayersVO]addLayer(layerNumber: "+ (layerNumber == -1) ? "New Layer(Num in nect log)" : layerNumber +")");
+		console.debug("[CostumeLayersNewVO]addLayer(layerNumber: "+ (layerNumber == -1) ? "New Layer(Num in nect log)" : layerNumber +")");
 		if(layerNumber == -1){
 			this['_layer' + (++this._amount)] = sprite;
 			this['_layer' + this._amount].name = 'layer' + (this._amount);
-			console.debug("[CostumeLayersVO]addLayer() -> Layers in costume = " + this._amount);
+			console.debug("[CostumeLayersNewVO]addLayer() -> Layers in costume = " + this._amount);
 			return this['_layer' + this._amount];
 		}
 	};
+	//Dynamic layers are ones that have multiple animations inside of it
 	p.isADynamicLayer = function(typeToCheck){
-		if (typeToCheck == sjs.CostumeLayersVO.HEAD ||
-			typeToCheck == sjs.CostumeLayersVO.EYES ||
-			typeToCheck == sjs.CostumeLayersVO.LIDS) {
+		if (typeToCheck == sjs.CostumeLayersNewVO.EYES){
 		   return true;
 		};
 	};
@@ -65,14 +56,25 @@ this.sjs = window.sjs || {};
 	 * @return {String} a string representation of the instance.
 	 **/
 	p.toString = function() {
-		return "CostumeLayersVO{_amount:" + this._amount + "}";
+		return "CostumeLayersNewVO{_amount:" + this._amount + "}";
 	};
 	
 	// private methods:
-	sjs.CostumeLayersVO = CostumeLayersVO;
-	sjs.CostumeLayersVO.BODY = "body";
-	sjs.CostumeLayersVO.HEAD = "head";
-	sjs.CostumeLayersVO.EYES = "eyes";
-	sjs.CostumeLayersVO.LIDS = "lids";
+	sjs.CostumeLayersNewVO = CostumeLayersNewVO;
+	sjs.CostumeLayersNewVO.buildLayer = function(target, sprite, layerNumber = -1){
+		if(!sprite){
+			console.warn("[CostumeLayersNewVO] NO SPRITE WAS GIVEN for method 'addLayer'. Stoping method..");
+			return;
+		}
+		layerNumber = (layerNumber == -1) ? target.children.length+1 : layerNumber;
+		sprite.name = 'layer' + (layerNumber);
+		return sprite;
+	};
+	sjs.CostumeLayersNewVO.BODY = "body";
+	sjs.CostumeLayersNewVO.HEAD = "head";
+	sjs.CostumeLayersNewVO.EYES = "eyes";
+	sjs.CostumeLayersNewVO.LIDS = "lids";
+	sjs.CostumeLayersNewVO.EYES_WITH_LID = "lidsInEyeSS";
+	sjs.CostumeLayersNewVO.EXTRA = "extra";
 }());
  
